f967e2b5f7ea3136d8067e2b49c2cab1201c35be
AndresSp <andrestunonsp@gmail.com>
2020-02-10 02:37:23 +0000

    Initial Modules with Permissions Setup (2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, PermissionsModule, RolesModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
